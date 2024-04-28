892d508c232b47cbaf266ebedc38dca0746de0b5
るるまる <88281866+sugoruru@users.noreply.github.com>
2024-04-28 14:35:29 +0900

    Add files via upload

---
+++ cppLib.h
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+using namespace chrono;
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (n); ++i)
+#define rrep(i, n) for (ll i = n; i > 0; --i)
+#define bitrep(i, n) for (ll i = 0; i < (1 << n); ++i)
+#define all(a) (a).begin(), (a).end()
+#define yesNo(b) ((b) ? "Yes" : "No")
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const double pi = 3.141592653589793;
+ll smallMOD = 998244353;
+ll bigMOD = 1000000007;
+ll dx[] = {1, 0, -1, 0, 1, -1, -1, 1};
+ll dy[] = {0, 1, 0, -1, 1, 1, -1, -1};
